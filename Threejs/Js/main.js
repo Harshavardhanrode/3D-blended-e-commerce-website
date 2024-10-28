096ca3e0e97c85b328607192ce2b459ba75d847c
Harsh_rode <rodeharsh.0027@gmail.com>
2024-10-28 13:05:26 +0530

    added an object(sandisk pandrive)

--- Threejs/Js/main.js
+++ Threejs/Js/main.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 
 
 
-let hashmap = new Map([[1,"logitech Keyboard"],[2,"8kg dumbells(4kg each)"],[3,"iphone 13 pro max"],[4,"MI Smart TV (32 inch)"],[5," golden Wine glass set(6 pieces)"],[6," china metal bottles"],[7,"MI Powerbank (20000 MAH)"],[8,"Hydro flask"],[9,"Steel Hammer"],[12,"Hydro flask"],[10,"wooden table with glass plate"],[11,"Steel Hammer"],[20,"MI Smart TV (32 inch)"]]);
+let hashmap = new Map([[1,"logitech Keyboard"],[2,"8kg dumbells(4kg each)"],[3,"iphone 13 pro max"],[4,"MI Smart TV (32 inch)"],[5," golden Wine glass set(6 pieces)"],[6," china metal bottles"],[7,"MI Powerbank (20000 MAH)"],[8,"Hydro flask"],[9,"Steel Hammer"],[12,"Hydro flask"],[10,"wooden table with glass plate"],[11,"Steel Hammer"],[20,"MI Smart TV (32 inch)"],[13,"SONATA Alarm clock"],[14,"Sandisk dual pendrive(128gb)"]]);
 
 const imgNo = sessionStorage.getItem('img_no');
 document.querySelector('h1').innerHTML=hashmap.get(Number(imgNo));
