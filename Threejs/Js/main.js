b5780480a69f9d99be11f2cfe0463d9139d9f3e8
Harsh_rode <rodeharsh.0027@gmail.com>
2024-10-28 20:53:52 +0530

    added an object(plywood) and texture images

--- Threejs/Js/main.js
+++ Threejs/Js/main.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 
 
 
-let hashmap = new Map([[1,"logitech Keyboard"],[2,"8kg dumbells(4kg each)"],[3,"iphone 13 pro max"],[4,"MI Smart TV (32 inch)"],[5," golden Wine glass set(6 pieces)"],[6," china metal bottles"],[7,"MI Powerbank (20000 MAH)"],[8,"Hydro flask"],[9,"Steel Hammer"],[12,"Hydro flask"],[10,"wooden table with glass plate"],[11,"Steel Hammer"],[20,"MI Smart TV (32 inch)"],[13,"SONATA Alarm clock"],[14,"Sandisk dual pendrive(128gb)"],[15,"SAMSUNG galaxy A05(128gb)"],[16,"Cactus plant"],[17,"prestage Knife set"]]);
+let hashmap = new Map([[1,"logitech Keyboard"],[2,"8kg dumbells(4kg each)"],[3,"iphone 13 pro max"],[4,"MI Smart TV (32 inch)"],[5," golden Wine glass set(6 pieces)"],[6," china metal bottles"],[7,"MI Powerbank (20000 MAH)"],[8,"Hydro flask"],[9,"Steel Hammer"],[12,"Hydro flask"],[10,"wooden table with glass plate"],[11,"Steel Hammer"],[20,"MI Smart TV (32 inch)"],[13,"SONATA Alarm clock"],[14,"Sandisk dual pendrive(128gb)"],[15,"SAMSUNG galaxy A05(128gb)"],[16,"Cactus plant"],[17,"prestage Knife set"],[18,"plywood rolls(1m x 10m)"]]);
 
 const imgNo = sessionStorage.getItem('img_no');
 document.querySelector('h1').innerHTML=hashmap.get(Number(imgNo));
